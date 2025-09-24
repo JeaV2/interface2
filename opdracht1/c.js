aea0b249c85ea0c4b07d6d62776e26d3cddfc683
JeaV2 <102710@glr.nl>
2025-09-24 14:46:31 +0200

    Opdracht 1 c

---
+++ opdracht1/c.js
@@ -0,0 +1,4 @@
+const pi = 3.14159;
+
+pi = 3.14; // Dit geeft een error omdat je een const niet kunt reassignen.
+// Een const gebruik je eigenlijk alleen voor waardes die nooit veranderen. En waardes die je niet wilt dat ze veranderen.
