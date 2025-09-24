8379b8c7623270849a7d8cf58081a6b409e82f6c
JeaV2 <102710@glr.nl>
2025-09-24 14:37:48 +0200

    Opdracht 1

---
+++ opdracht1/a.js
@@ -0,0 +1,10 @@
+let count = 0;
+count += 5;
+
+const firstName = "Jane";
+
+console.log(`Hello, ${firstName}`);
+console.log(`Count is: ${count}`);
+
+// Dit geeft een error omdat je een const niet kunt reassignen.
+// firstName = "Doe";
