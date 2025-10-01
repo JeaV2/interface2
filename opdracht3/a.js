d13379467f04bcf7cf64b875adca8d05b4531cba
JeaV2 <102710@glr.nl>
2025-10-01 14:11:49 +0200

    Opdracht 3

---
+++ opdracht3/a.js
@@ -0,0 +1,6 @@
+let naam = "Jane";
+let klas = "2E";
+let opleiding = "Creative Software Developer";
+const vandaag = new Date().toLocaleDateString("nl-NL");
+
+console.log(`Welkom ${naam}! Je zit in de klas: ${klas}, op de opleiding: ${opleiding}.\n Vandaag is het ${vandaag}.`);
---
+++ opdracht3/b.js
@@ -0,0 +1,6 @@
+const product = "Tosti";
+const prijs = 2.75;
+const aantal = 3;
+
+console.log(`Je hebt ${aantal} ${product}(s) besteld. De prijs per stuk is €${prijs.toFixed(2)}.\nDe totale prijs is €${(prijs * aantal).toFixed(2)}.`);
+
